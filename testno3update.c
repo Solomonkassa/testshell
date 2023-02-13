798931b8c6675819003abbfe1a8b1f7042db27d7
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-02-12 19:53:41 -0500

    Create testno3update.c

---
+++ testno3update.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+
+extern char **environ;
+
+char *_strtok(char *str, const char *delim)
+{
+    static char *last;
+    char *nextp;
+
+    if (str != NULL)
+    {
+        last = str;
+    }
+
+    if (*last == '\0')
+    {
+        return NULL;
+    }
+
+    char *p = strpbrk(last, delim);
+
+    if (p == NULL)
+    {
+        nextp = strchr(last, '\0');
+    }
+    else
+    {
+        *p = '\0';
+        nextp = p + 1;
+    }
+
+    char *token = last;
+    last = nextp;
+
+    return token;
+}
+
+char *_getline()
+{
+    char c;
+    int i = 0;
+    int size = 80;
+
+    char *buffer = malloc(size * sizeof(char));
+    if (buffer == NULL)
+    {
+        return NULL;
+    }
+
+    while ((c = getchar()) != '\n' && c != EOF)
+    {
+        buffer[i++] = c;
+
+        if (i == size)
+        {
+            size *= 2;
+            char *temp = realloc(buffer, size * sizeof(char));
+
+            if (temp == NULL)
+            {
+                free(buffer);
+                return NULL;
+            }
+            buffer = temp;
+        }
+    }
+    buffer[i] = '\0';
+    return buffer;
+}
+
+int main(int argc, char *argv[])
+{
+    char *token;
+    char *why;
+    char *token1 = NULL;
+    char *token0 = NULL;
+    char *s = malloc (80 * sizeof(char));
+    int i = 0;
+    char *u = getcwd(s, 80 * sizeof(char)) ;
+    while (1)
+    {
+        printf("#cisfun$ ");
+        why = _getline();
+
+        if (feof(stdin))
+        {
+            free(why);
+            exit(1);
+        }
+
+        token = _strtok(why, " ");
+
+        if (strcmp(token, "exit") == 0)
+        {
+            free(why);
+            break;
+        }
+
+        i = 0;
+        while (token != NULL && i < 2)
+        {
+           if (i == 0)
+            {
+            token0 = token;
+            }
+          if (i == 1)
+            {
+                token1 = token;
+            }
+            i++;
+            token = _strtok(NULL, " ");
+        }
+      
+  
+        
+        if (strcmp(token0, "cd") == 0)
+        {
+            chdir(token1);
+            continue;
+        }
+        if (strcmp(token0,"pwd") == 0)
+        {
+      
+           if (getcwd(s, 80 * sizeof(char)) != NULL)
+           {
+           printf("%s\n", s);
+           }
+           else
+           {
+           perror("getcwd() error");
+           }
+          continue;
+         }
+         if (strcmp(token0,"ls") == 0)
+         {
+           char *n[100] = {"/bin/ls",token1,NULL};
+           execve (n[0],n,environ);
+           perror ("ls");
+           continue;
+         }
+        {
+            pid_t pid = fork();
+
+            if (pid < 0)
+            {
+                perror("fork");
+                free(why);
+                continue;
+            }
+            else if (pid == 0)
+            {
+                char *arg[] = {token0,NULL};
+
+                execve(arg[0],arg,environ),
+            perror("execve");
+                free(why);
+                exit(1);
+            }
+            else
+            {
+                wait(NULL);
+            }
+        }
+
+        free(why);
+    }
+
+    return 0;
+}
