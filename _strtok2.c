2a79ae63289e71b6f1201d23823196c5cb77c3a2
Nawwal Ismael <118599845+Newi-M@users.noreply.github.com>
2023-02-16 11:18:11 +0300

    Update _strtok2.c

--- _strtok2.c
+++ _strtok2.c
@@ -5,23 +5,32 @@ char* _strtok(char* str, const char* delim)
   static char* last;
   char* token;
 
-  if (str != NULL) {
+  if (str != NULL)
+  {
     last = str;
   }
 
-  token = last;//Hello World....{" ?,"}
-
-  while (*last != '\0') {
-    for (const char* d = delim; *d != '\0'; ++d) {
-      if (*last == *d) {
-        *last++ = '\0';
-        return token;
-      }
-    }
-    ++last;
+  if (*last == '\0')
+  {
+    return NULL;
   }
 
-  return NULL;
+  token = last;
+
+  while (*last != '\0')
+  {
+    const char* d = delim;
+    for (const char *d = delim; *d != '\0'; d++)
+    {
+      if (*last == *d)
+      {
+        *last = '\0';
+        last++;
+        return (token);
+      }
+    }
+    last++;
+  }
+
+  return (token);
 }
-
-
